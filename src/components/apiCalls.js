52f56455526505f1f59616644a6b13bd88664eaf
Janika Hortizuela <jhortizuela21@gmail.com>
2021-11-15 20:05:22 -0700

    update api calls

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -1,11 +1,11 @@
 export const fetchParentData = () => {
-  return fetch('http://localhost:8080/api/v1/parents/parentData/')
+  return fetch('https://cool-parent-api.herokuapp.com/api/v1/parents/parentData/')
     .then(response => response.json())
     .catch(err => console.log(err))
 }
 
 export const fetchEventData = () => {
-  return fetch('http://localhost:8080/api/v1/parents/eventData/')
+  return fetch('https://cool-parent-api.herokuapp.com/api/v1/parents/eventData/')
     .then(response => response.json())
     .catch(err => console.log(err))
 }
