91e304159a11c38c72e47fc30364e8982cb6fffb
Janika Hortizuela <jhortizuela21@gmail.com>
2021-11-28 13:41:44 -0700

    Finished menu styling and added links

--- src/LandingPageComponents/Nav.js
+++ src/LandingPageComponents/Nav.js
@@ -1,15 +1,40 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../LandingPageStyles/Nav.scss'
+import 'animate.css';
 
 const Nav = () => {
   const [menuOpen, setMenuOpenState] = useState(false)
 
-  const handleMenuClick 
+  const handleMenuClick = () => {
+    menuOpen ? setMenuOpenState(false) : setMenuOpenState(true)
+  }
+
+  const menuItems = menuOpen ?
+    <div className="items">
+      <Link to='/'>
+        <button className="menu-btn animate__animated animate__fadeInDown">Home</button>
+      </Link>  
+      <Link to='/profiles'>
+        <button className="menu-btn animate__animated animate__fadeInDown">Profiles</button> 
+      </Link>
+      <Link to='/events'>
+        <button className="menu-btn animate__animated animate__fadeInDown">Events</button>
+      </Link>
+    {/* how it works needs to be fixed */}
+      <button className="menu-btn animate__animated animate__fadeInDown">How It Works</button>
+    {/* need to make meet the cretor  */}
+      <button className="menu-btn animate__animated animate__fadeInDown">Meet the Creator</button>
+    </div>
+    : null
 
   return (
     <div className="landing-nav">
       <h1 className="nav-title">Cool Parent</h1>
-      <button className="menu">MENU</button>
+      <div>
+        <button className="menu" onClick={handleMenuClick}>MENU</button>
+        {menuItems}
+      </div>
     </div>
   )
 }
